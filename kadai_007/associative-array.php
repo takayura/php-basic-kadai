40204aad909db99b4562aafe0beb60171a624723
鷹羽ゆら <162697800+takayura@users.noreply.github.com>
2024-05-14 13:22:07 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列を代入する
+    $array = [
+      'name' => 'onion',
+      'price' => 200,
+      'weight' => 160
+    ];
+    // 連想配列の値を出力する
+    print_r($array);
+    ?>
+  </p>
+</body>
+
+</html>
+<!--「localhost/開きたいファイルが入っているフォルダ名/ファイル名」 -->
